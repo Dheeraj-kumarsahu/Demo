11d16a2b58856d9c750595d64f7067185bce93ed
Dheerajkumarsahu <dheerajkumarsahu827@gmail.com>
2026-06-13 13:07:26 +0530

    Basic Buttons

---
+++ P.py
@@ -0,0 +1,22 @@
+#Author :-Dheeraj kumar sahu
+# Purpose :- Simple Calculator 
+#Contact :- +917389580688
+import tkinter as tk
+
+#creating the Window
+window=tk.Tk()
+window.title("Simple Calculator")
+window.geometry("500x500")
+window.configure(bg="DarkBlue")
+
+#Creating the buttons
+button0=tk.Button(window,text="0",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button1=tk.Button(window,text="1",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button2=tk.Button(window,text="2",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button3=tk.Button(window,text="3",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button4=tk.Button(window,text="4",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button5=tk.Button(window,text="5",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button6=tk.Button(window,text="6",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button7=tk.Button(window,text="7",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button8=tk.Button(window,text="8",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
+button9=tk.Button(window,text="9",width=5,height=2,font=("Times New Roman",20),bg="LightBlue")
